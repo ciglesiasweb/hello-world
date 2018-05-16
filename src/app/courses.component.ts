cc127c9a4a6ff8a74144e8de7b7f4d8b916f2e02
Carlos Iglesias <carlos.iglesias@optivamedia.com>
2018-05-16 14:05:06 +0200

    bindings

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -6,13 +6,30 @@ import {Component} from '@angular/core';
     template: `
         <h2>{{ getTitle() }}</h2>
         <ul>
-            <li *ngFor="let course of courses">{{course}}</li>
-        </ul>`
-
+        <li *ngFor="let course of courses">{{course}}</li>
+        </ul>
+        <table>
+        <tr>
+        <td [attr.colspan]="colSpan"> tttt</td> 
+        </tr>
+        </table>
+        <img [src]="imageUrl"/>
+        <button class="btn btn-primary" [class.active] = "isActive" [style.backgroundColor] = "isActive ? 'blue' : 'red'">Press me!!</button>
+        <button (click)= "onSave($event)">Save </button>
+        <input (keyup.enter) = "onKeyUp()" />
+        `
+        
 })
 
 export class CoursesComponent{
     title = "List of courses"
+    // property binding
+    imageUrl = "//cdn.shopify.com/s/files/1/0986/5790/products/HelloDecal-PRINT_1024x1024.png?v=1481472974"
+    // attribute binding
+    colSpan = 2;
+    // class binding
+    isActive = true;
+    // style binding
     courses;
 
     constructor(service:CoursesService){
@@ -20,9 +37,16 @@ export class CoursesComponent{
     }
 
 
+    onSave($event){
+        console.log('Button was clicked', $event)
+    }
 
     getTitle(){
         return this.title
+    }
+
+    onKeyUp(){
+        console.log('ENTER key was pressed')
     }
 
 }
